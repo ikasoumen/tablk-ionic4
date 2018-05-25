208a5e2907541c106455556ad3e86a9ebb74a391
ikasoumen <yukinari3po@gmail.com>
2018-05-26 02:52:10 +0900

    add modal

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { NavController, ModalController, AlertController } from '@ionic/angular';
+import { AboutPage } from '../about/about.page';
 
 @Component({
   selector: 'app-page-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+  constructor(
+    public navCtrl: NavController,
+    public modalCtrl: ModalController,
+    public alertCtrl: AlertController,
+  ) {}
+
+  public async pushHomePage() {
+    // this.navCtrl.goForward('about');
+    console.log('comes here');
+    const modal = await this.modalCtrl.create({ component: AboutPage });
+    modal.present();
+    // this.alertCtrl.create({});
+  }
+}
