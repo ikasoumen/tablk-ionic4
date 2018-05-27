84312ef4125fb97b9239c3164ddf39351319d669
ikasoumen <yukinari3po@gmail.com>
2018-05-28 04:26:05 +0900

    fix

--- src/app/http/api/default.service.ts
+++ src/app/http/api/default.service.ts
@@ -21,10 +21,10 @@ import {
 } from "@angular/common/http";
 import { CustomHttpUrlEncodingCodec } from "../encoder";
 
-import { Observable } from "rxjs/Observable";
+import { Observable } from "rxjs";
 
 import { ApiKeyResponse } from "../model/apiKeyResponse";
-import { ApikeyInput } from "../model/apikeyInput";
+import { ApiKeyInput } from "../model/apiKeyInput";
 import { ErrorResponse } from "../model/errorResponse";
 import { SessionCreateInput } from "../model/sessionCreateInput";
 import { SessionDeleteInput } from "../model/sessionDeleteInput";
@@ -81,22 +81,22 @@ export class DefaultService {
    * @param reportProgress flag to report request and response progress.
    */
   public authApiKeyPost(
-    apiKeyInput: ApikeyInput,
+    apiKeyInput: ApiKeyInput,
     observe?: "body",
     reportProgress?: boolean
   ): Observable<ApiKeyResponse>;
   public authApiKeyPost(
-    apiKeyInput: ApikeyInput,
+    apiKeyInput: ApiKeyInput,
     observe?: "response",
     reportProgress?: boolean
   ): Observable<HttpResponse<ApiKeyResponse>>;
   public authApiKeyPost(
-    apiKeyInput: ApikeyInput,
+    apiKeyInput: ApiKeyInput,
     observe?: "events",
     reportProgress?: boolean
   ): Observable<HttpEvent<ApiKeyResponse>>;
   public authApiKeyPost(
-    apiKeyInput: ApikeyInput,
+    apiKeyInput: ApiKeyInput,
     observe: any = "body",
     reportProgress: boolean = false
   ): Observable<any> {
