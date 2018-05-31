58c97cc63190bcaab16ca78f3e41ec180c1da1f5
ikasoumen <yukinari3po@gmail.com>
2018-05-31 20:51:11 +0900

    update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: "", loadChildren: "./pages/tabs/tabs.module#TabsPageModule" }
+  {
+    path: "",
+    loadChildren: "./pages/noLogin/noLogin.module#NoLoginPageModule",
+    pathMatch: "full"
+  },
+  {
+    path: "noLogin",
+    loadChildren: "./pages/noLogin/noLogin.module#NoLoginPageModule"
+  }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
