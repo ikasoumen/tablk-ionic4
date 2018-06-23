f1ebcd9623e4ed864eb1d57875cb98ea2ad5b2fe
ikasoumen <yukinari3po@gmail.com>
2018-06-23 18:35:00 +0900

    add helper

---
+++ src/app/helpers/helpers.ts
@@ -0,0 +1,16 @@
+export function setSome<K extends keyof T, T>(
+  map: Map<K, T>,
+  dictionary: Iterable<[K, T]>
+): Map<K, T> {
+  for (const [key, value] of dictionary) {
+    map.set(key, value);
+  }
+  return map;
+}
+
+export function addSome<T>(set: Set<T>, values: T[]): Set<T> {
+  for (const value of values) {
+    set.add(value);
+  }
+  return set;
+}
