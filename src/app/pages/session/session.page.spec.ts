0e3dc8d220aa818236c498dd2a6c8ca7da9c1109
ikasoumen <yukinari3po@gmail.com>
2018-06-24 02:41:58 +0900

    update

--- src/app/pages/session/session.page.spec.ts
+++ src/app/pages/session/session.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { SessionComponent } from "./session.component";
+import { SessionPage } from "./session.page";
 
-describe("SessionComponent", () => {
-  let component: SessionComponent;
-  let fixture: ComponentFixture<SessionComponent>;
+describe("SessionPage", () => {
+  let component: SessionPage;
+  let fixture: ComponentFixture<SessionPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SessionComponent]
+      declarations: [SessionPage]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SessionComponent);
+    fixture = TestBed.createComponent(SessionPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/pages/session/session.page.ts
+++ src/app/pages/session/session.page.ts
@@ -8,13 +8,13 @@ import { SessionsStore } from "app/stores/sessions.store";
 import { Observable, of } from "rxjs";
 import { ActivatedRoute } from "@angular/router";
 import { Session } from "app/http";
+import { InputChangeEvent } from "@ionic/core";
 
-namespace SessionPage {
-  export interface Params {
-    id: string;
-  }
+enum segments {
+  Description = "Description",
+  Members = "Members",
+  Notes = "Notes"
 }
-
 @Component({
   selector: "tablk-session-page",
   templateUrl: "./session.page.html",
@@ -24,14 +24,29 @@ namespace SessionPage {
 })
 export class SessionPage implements OnInit {
   public session$: Observable<{ exist: boolean; _: Session }>;
+  public selectedSegment: segments = segments.Description;
   private id: string;
 
   constructor(private route: ActivatedRoute, private sessions: SessionsStore) {}
 
-  ngOnInit() {
+  public ngOnInit() {
     this.route.params.subscribe((params: SessionPage.Params) => {
       this.id = params.id;
     });
     this.session$ = this.sessions.readOne$(of(this.id));
+  }
+
+  public get segment() {
+    return segments;
+  }
+
+  public selectSegment(event: CustomEvent) {
+    this.selectedSegment = event.detail.value;
+  }
+}
+
+namespace SessionPage {
+  export interface Params {
+    id: string;
   }
 }
